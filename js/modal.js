32bffa897aca65ea9493a9853aa5a02b5c44df06
Yuliia Lytvynenko <mikyjee.db@gmail.com>
2025-05-02 10:54:32 +0300

    Мій перший комміт

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,34 @@
 (() => {
     const refs = {
-        // Додати атрибут data-modal-open на кнопку відкриття
         openModalBtn: document.querySelector("[data-modal-open]"),
-        // Додати атрибут data-modal-close на кнопку закриття
         closeModalBtn: document.querySelector("[data-modal-close]"),
-        // Додати атрибут data-modal на бекдроп модалки
         modal: document.querySelector("[data-modal]"),
+        burgerBtn: document.querySelector("#toggleButton"),
+        mobMenu: document.querySelector("#mob-menu"),
     };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+    // Модальне вікно (Order Service)
+    if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+        refs.openModalBtn.addEventListener("click", toggleModal);
+        refs.closeModalBtn.addEventListener("click", toggleModal);
+    }
 
     function toggleModal() {
-        // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
         refs.modal.classList.toggle("is-open");
+    }
+
+    // Мобільне меню (бургер кнопка)
+    if (refs.burgerBtn && refs.mobMenu) {
+        refs.burgerBtn.addEventListener("click", () => {
+            refs.mobMenu.classList.toggle("is-open-mob");
+        });
+
+        // Закриття моб. меню по хрестику
+        const closeBtn = document.querySelector("#close-btn");
+        if (closeBtn) {
+            closeBtn.addEventListener("click", () => {
+                refs.mobMenu.classList.remove("is-open-mob");
+            });
+        }
     }
 })();
